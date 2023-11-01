776499b9d567b1e5791bbf7e799ca60937ad77de
Edvan Ribeiro <jk.jr47@gmail.com>
2023-10-31 23:38:18 -0300

    exercises Difference Of Squares

--- csharp/difference-of-squares/DifferenceOfSquares.cs
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -1,19 +1,16 @@
 using System;
+using System.Linq;
 
 public static class DifferenceOfSquares
 {
-    public static int CalculateSquareOfSum(int max)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
 
-    public static int CalculateSumOfSquares(int max)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int CalculateSquareOfSum(int max) => (int)Pow(ArithmeticProgression(1,max),2);
 
-    public static int CalculateDifferenceOfSquares(int max)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int CalculateSumOfSquares(int max) => (int)Enumerable.Range(1,max).Select((n) => Pow(n,2)).Sum();
+    public static int CalculateDifferenceOfSquares(int max) => CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+
+    private static int ArithmeticProgression(int start,int max) => (start+max)*max/2;
+
+    private static double Pow(int value,int raised) => Math.Pow(value,raised);
+
 }
